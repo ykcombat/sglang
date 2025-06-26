c1dcab2dc5539b3f1bcaafb2b639b38b7a69b149
ykcombat <raphaelhao@outlook.com>
2025-06-26 10:52:40 +0000

    add greenctx stream to sgl-kernel

--- sgl-kernel/python/sgl_kernel/__init__.py
+++ sgl-kernel/python/sgl_kernel/__init__.py
@@ -72,6 +72,10 @@ from sgl_kernel.speculative import (
     tree_speculative_sampling_target_only,
     verify_tree_greedy,
 )
+from sgl_kernel.spatial import(
+    create_greenctx_stream_by_value,
+    get_sm_available,
+)
 from sgl_kernel.top_k import fast_topk
 from sgl_kernel.version import __version__
 
